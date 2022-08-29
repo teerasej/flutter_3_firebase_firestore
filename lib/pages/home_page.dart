e326f3a94d162cdc4e9aa8f62c04ea48b6afca2d
teerasej <teerasej@gmail.com>
2022-08-29 22:49:10 +0700

    load and show data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_3_firebase_firestore/models/item_model.dart';
 import 'package:flutter_3_firebase_firestore/utils/route_map.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  var _db = FirebaseFirestore.instance;
+  List<ItemModel> _itemModels = [];
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +23,32 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: FutureBuilder(
+        future: _db.collection('items').get(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.hasData) {
+            var docs = snapshot.data!.docs;
+            _itemModels = docs.map<ItemModel>(
+              (e) {
+                return ItemModel.fromMap(e.data());
+              },
+            ).toList();
+
+            return ListView.builder(
+              itemCount: _itemModels.length,
+              itemBuilder: (BuildContext context, int index) {
+                var item = _itemModels[index];
+                return ListTile(
+                  title: Text(item.name),
+                );
+              },
+            );
+          }
+
+          return LinearProgressIndicator();
+        },
+      ),
     );
   }
 }
