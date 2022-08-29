59ff2bc89834934d3111d91a55955a17d15b126e
teerasej <teerasej@gmail.com>
2022-08-29 22:24:55 +0700

    prepare ui for new item page

--- lib/pages/new_item_page.dart
+++ lib/pages/new_item_page.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
 class NewItemPage extends StatelessWidget {
-  const NewItemPage({Key? key}) : super(key: key);
+  NewItemPage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+  String _newItemName = '';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Item'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Text('Name:'),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value != null && value.isNotEmpty) {
+                  return null;
+                } else {
+                  return 'Please fill the form';
+                }
+              },
+              onSaved: (value) {
+                _newItemName = value ?? '';
+              },
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
