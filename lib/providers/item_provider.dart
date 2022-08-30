4cbf167d9ef0a40065b65fde032ca613928b8a83
teerasej <teerasej@gmail.com>
2022-08-30 21:09:56 +0700

    create provider

---
+++ lib/providers/item_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_3_firebase_firestore/models/item_model.dart';
+
+class ItemProvider extends ChangeNotifier {
+  final _db = FirebaseFirestore.instance;
+
+  ItemModel? editingItem;
+}
